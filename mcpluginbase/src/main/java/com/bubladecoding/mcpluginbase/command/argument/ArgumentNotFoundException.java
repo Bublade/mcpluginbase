303e5fde17912380d2e879b0074e07a9956ab1f4
Bublade <alexthvb@gmail.com>
2020-10-05 15:15:36 +0200

    Command simple argument parser

---
+++ mcpluginbase/src/main/java/com/bubladecoding/mcpluginbase/command/argument/ArgumentNotFoundException.java
@@ -0,0 +1,50 @@
+package com.bubladecoding.mcpluginbase.command.argument;
+/*
+ * Copyright (c) 2020 bublade
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+public class ArgumentNotFoundException extends RuntimeException {
+
+    private static final String message = "Argument at position: %d not found.";
+
+    public ArgumentNotFoundException() {
+    }
+
+    public ArgumentNotFoundException(int position) {
+        super(format(position));
+    }
+
+    public ArgumentNotFoundException(int position, Throwable cause) {
+        super(format(position), cause);
+    }
+
+    public ArgumentNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public ArgumentNotFoundException(int position, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(format(position), cause, enableSuppression, writableStackTrace);
+    }
+
+    private static String format(int position) {
+        return String.format(message, position);
+    }
+}
